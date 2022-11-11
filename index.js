403c3c2517f8c8e4c3861c08a364bb86bba359be
Edye1230 <edye.1230@gmail.com>
2022-11-10 20:01:52 -0500

    Levantando servidor básico

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const http = require("http");
+
 //import os from "os";
 
 console.log("Hola mundo!");
 
 //console.log(os.platform());
+
+const server = http.createServer((req, res) => {
+    res.status = 200;
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hola mundo");
+});
+
+server.listen(3000, () => {
+    console.log("Servidor prendido");
+});
